46647084ef9622af22fd6c209ce86e471b3edc71
Gemei <hosam.gemeai@gmail.com>
2025-06-18 11:34:56 -0600

    Fixed configuration validation bugs

--- modules/utils.py
+++ modules/utils.py
@@ -19,6 +19,17 @@ def custom_serializer(obj):
         return obj.isoformat()
     raise TypeError(f"Type {type(obj)} not serializable")
 
+def has_attacker_creds():
+    return bool(my_globals.attacker_access_key and my_globals.attacker_secret_access_key)
+
+def has_victim_creds():
+    return bool(my_globals.victim_access_key and my_globals.victim_secret_access_key)
+
+def validate_wordlist(path, name):
+    if not os.path.isfile(path):
+        return f"[#] Supplied {name} wordlist '{path}' doesn't exist or is inaccessible. Scan cannot run!"
+    return None
+
 def validate_config(config):
     my_globals.start_username_brute_force = config.get("start_username_brute_force") or False
     my_globals.start_role_name_brute_force = config.get("start_role_name_brute_force") or False
@@ -39,7 +50,6 @@ def validate_config(config):
     my_globals.attacker_IAM_role_name = config.get("attacker_IAM_role_name") or None
     my_globals.attacker_S3_role_arn = config.get("attacker_S3_role_arn") or None
 
-    # Limit regions to only selected regions
     my_globals.victim_regions = config.get("victim_regions") or None
     if my_globals.victim_regions:
         my_globals.aws_regions = my_globals.victim_regions
@@ -47,7 +57,7 @@ def validate_config(config):
     warnings = []
     errors = []
 
-    if not my_globals.attacker_access_key and not my_globals.attacker_secret_access_key:
+    if not has_attacker_creds():
         warnings.append("[*] Attacker credentials not provided. The following features will be disabled:\n"
                         "   - Searching for public EBS snapshots using victim account ID\n"
                         "   - Brute-forcing AWS account IDs via exposed S3 or access key IDs\n"
@@ -59,7 +69,7 @@ def validate_config(config):
     if not my_globals.attacker_S3_role_arn and not my_globals.victim_buckets:
         warnings.append("[*] No attacker S3 role or victim buckets provided. Cannot brute-force victim AWS account ID from public S3 buckets.")
 
-    if not my_globals.victim_access_key and not my_globals.victim_secret_access_key:
+    if not has_victim_creds():
         warnings.append("[*] Victim credentials not provided. Only unauthenticated enumeration will be performed.")
     elif not my_globals.victim_secret_access_key:
         warnings.append("[*] Victim secret access key missing. Only basic account-level information can be retrieved.")
@@ -68,25 +78,27 @@ def validate_config(config):
         warnings.append("[*] Victim AWS account ID was not provided. Public EBS snapshot enumeration might be skipped if sts call failed.")
 
     if not my_globals.start_username_brute_force:
-        warnings.append("[*] Username brute-force set to \"false\" in the \"enum.config.json\" config file.")
+        warnings.append("[*] Username brute-force set to 'false' in the 'enum.config.json' config file.")
 
     if not my_globals.start_role_name_brute_force:
-        warnings.append("[*] Role name brute-force set to \"false\" in the \"enum.config.json\" config file.")
+        warnings.append("[*] Role name brute-force set to 'false' in the 'enum.config.json' config file.")
 
-    if not os.path.isfile(my_globals.user_name_wordlist) and my_globals.start_username_brute_force:
-        errors.append(f"[#] Supplied usernames wordlist {my_globals.user_name_wordlist} doesn't exist or inaccessible, check config in \"enum.config.json\" file."
-                      f" Scan cannot run!")
+    if my_globals.start_username_brute_force:
+        result = validate_wordlist(my_globals.user_name_wordlist, "username")
+        if result:
+            errors.append(result)
 
-    if not os.path.isfile(my_globals.role_name_wordlist) and my_globals.start_role_name_brute_force:
-        errors.append(f"[#] Supplied usernames wordlist {my_globals.role_name_wordlist} doesn't exist or inaccessible, check config in \"enum.config.json\" file."
-                      f" Scan cannot run!")
+    if my_globals.start_role_name_brute_force:
+        result = validate_wordlist(my_globals.role_name_wordlist, "role name")
+        if result:
+            errors.append(result)
 
-    if all(not x for x in [my_globals.attacker_access_key, my_globals.attacker_secret_access_key,
-                           my_globals.victim_secret_access_key, my_globals.victim_access_key]):
-        errors.append("[#] You didn't supply enough information in \"enum.config.json\" config file. Scan cannot run!")
+    if not any([my_globals.attacker_access_key, my_globals.attacker_secret_access_key,
+                my_globals.victim_access_key, my_globals.victim_secret_access_key]):
+        errors.append("[#] You didn't supply enough information in 'enum.config.json' config file. Scan cannot run!")
 
-    if all(not x for x in [my_globals.victim_access_key, my_globals.victim_buckets, my_globals.victim_aws_account_ID]):
-        errors.append("[#] You didn't supply enough information in \"enum.config.json\" config file. Scan cannot run!")
+    if not any([my_globals.victim_access_key, my_globals.victim_buckets, my_globals.victim_aws_account_ID]):
+        errors.append("[#] You didn't supply enough victim data in 'enum.config.json' config file. Scan cannot run!")
 
     for warning in warnings:
         print(f"{Fore.YELLOW}{warning}")
@@ -94,9 +106,4 @@ def validate_config(config):
     if errors:
         for error in errors:
             print(f"{Fore.RED}{error}")
-        sys.exit(0)
-
-    if not my_globals.victim_access_key:
-        return True
-    else:
-        return False
+        sys.exit(1)
